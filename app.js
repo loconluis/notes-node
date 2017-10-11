641bddef1864317eca8c775cd82dfb2ecd5711e5
loconluis <luis.locon@gmail.com>
2017-10-11 00:22:48 -0600

    Usando yargs para obtener informacion mas especifica de lo que se ingresa desde la entrada

--- app.js
+++ app.js
@@ -2,21 +2,30 @@ console.log('Start app.js!!!')
 
 const fs = require('fs')
 const _ = require('lodash')
+const yargs = require('yargs')
 
 const notes = require('./notes')
 
+const argv = yargs.argv
+
 let command = process.argv[2]
 console.log('Command: ', command)
-console.log(process.argv)
+// console.log('Process: \n', process.argv)
+// console.log('Yargs: \n', argv)
 
 if (command === 'add') {
-  console.log('Adding new note!')
+  // Add a new note
+  notes.addNote(argv.title, argv.body)
 } else if (command === 'list') {
-  console.log('Listing all notes!')
+  // List the notes
+  notes.listNotes()
 } else if (command === 'read') {
-  console.log('Reading note!')
+  // Read a single note
+  notes.readNote(argv.title)
 } else if (command === 'remove') {
-  console.log('Removing note!')
+  // Remove a single note
+  notes.removeNote(argv.title)
 } else {
+  // Not matching command
   console.log('Not recognized!')
 }
--- notes.js
+++ notes.js
@@ -1,10 +1,27 @@
 console.log('Starting note.js!')
 
-let addNote = () => {
-  console.log('Adding note!')
-  return 'New Note'
+// Adding note function
+let addNote = (title, body) => {
+  console.log('Adding note', title, body)
 }
 
+// List notes function
+let listNotes = () => {
+  console.log('Take a look for this notes \n >allnotes object here<')
+}
+
+// Remove a single note function
+let removeNote = (title) => {
+  console.log(`Removing note with title ${title}!`)
+}
+
+// Read a single note function
+let readNote = (title) => {
+  console.log(`Reading ${title} note`)
+}
 module.exports = {
-  addNote
+  addNote,
+  listNotes,
+  removeNote,
+  readNote
 }
