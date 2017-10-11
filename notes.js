8627268ac4b1d8233e03a8d77e0581df4e3338cf
loconluis <luis.locon@gmail.com>
2017-10-10 22:34:34 -0600

    Fundamentals of node

---
+++ notes.js
@@ -0,0 +1,18 @@
+console.log('Starting note.js!')
+
+let addNote = () => {
+  console.log('Adding note!')
+  return 'New Note'
+}
+
+function add(a, b) {
+  return a + b
+}
+
+module.exports = {
+  addNote,
+  add
+}
+
+
+
